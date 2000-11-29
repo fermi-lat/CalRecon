47db08eb5dd1e738c1c9b5edb36fc30990847177
burnett <>
2000-11-29 21:22:55 +0000

    Initial import of a skeleton. Does not work yet, since we need to extend the GlastDetSvc interface to be able to pass the constants from instrument.xml. Also, the CalData object needs to be defined from GlastEvent, and passed to CalRecon.

---
+++ CalRecon/mainpage.h
@@ -0,0 +1,14 @@
+// $Header: /nfs/slac/g/glast/ground/cvs/GlastSvc/GlastSvc/mainpage.h,v 1.2 2000/11/01 00:39:01 igable Exp $
+// Mainpage for doxygen
+
+/*! \mainpage package CalREcon
+
+
+  <hr>
+  \section notes release notes
+  \include release.notes
+  \section requirements requirements
+  \include requirements
+
+*/
+
