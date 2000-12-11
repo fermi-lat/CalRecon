6867d9eb8aaf2fe48fffc1a0ae7927480b952f5d
burnett <>
2000-12-11 16:39:22 +0000

    tidy up for release.

--- CalRecon/CalRecoAlg.h
+++ CalRecon/CalRecoAlg.h
@@ -1,12 +1,17 @@
-// $Header: $
+// $Header: /nfs/slac/g/glast/ground/cvs/CalRecon/CalRecon/CalRecoAlg.h,v 1.1.1.1 2000/11/29 21:22:55 burnett Exp $
 #ifndef CalRecoAlg_H
 #define CalRecoAlg_H
 
 // Include files
 #include "Gaudi/Algorithm/Algorithm.h"
 
+#include "reconstruction/SummaryData.h"
+
 // forward declarations
 class IGlastDetSvc;
+class CalRecon;
+class GlastTuple;
+
 
 /*! \class CalRecoAlg
 \brief Calorimeter reconstuction
@@ -29,6 +34,11 @@ public:
 private:
     // the GlastDetSvc used for access to detector info
     IGlastDetSvc*    m_detSvc;
+    // ptr to the CalRecon object used to do the analysis
+    CalRecon*    m_recon;
+
+    // sumamry object from glastsim creates a n-tuple
+    SummaryData<GlastTuple>* m_summary; 
 };
 
 
