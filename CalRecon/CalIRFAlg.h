0be3ef22f84dfeae05ffbfc0fa23e2ff6ef3422b
chehtman <>
2001-03-23 22:12:57 +0000

    Reconstruction of simulated IRF file with flight geometry added.

---
+++ CalRecon/CalIRFAlg.h
@@ -0,0 +1,55 @@
+// $Header: /nfs/slac/g/glast/ground/cvs/CalRecon/CalRecon/CalRecoAlg.h,v 1.4 2001/01/15 07:10:21 igable Exp $
+#ifndef CalIRFAlg_H
+#define CalIRFAlg_H
+
+// Include files
+#include "Gaudi/Algorithm/Algorithm.h"
+
+#include "reconstruction/SummaryData.h"
+#include "CalRecon/GaudiGlastTuple.h"
+
+// forward declarations
+class IGlastDetSvc;
+class CalRecon;
+class GlastTuple;
+namespace xml { class IFile; }
+
+
+/*! \class CalIRFAlg
+\brief Conversion calorimeter data from CsIData to CalRecLogs
+
+  */
+
+class CalIRFAlg : public Algorithm {
+
+public:
+  //! Constructor of this form must be provided
+  CalIRFAlg(const std::string& name, ISvcLocator* pSvcLocator); 
+
+  //! mandatory
+  StatusCode initialize();
+  //! mandatory
+  StatusCode execute();
+  //! mandatory
+  StatusCode finalize();
+
+  StatusCode printNewNTuple();
+
+private:
+    // the GlastDetSvc used for access to detector info
+    IGlastDetSvc*    m_detSvc;
+    // ptr to the CalRecon object used to do the analysis
+    CalRecon*    m_recon;
+
+    // constants from the "instrument.xml" file.
+    xml::IFile * m_ini;
+    
+    // sumamry object from glastsim creates a n-tuple
+//    SummaryData<GlastTuple>* m_summary;
+    SummaryData<GaudiGlastTuple>* m_gsummary;
+
+
+};
+
+
+#endif // CalIRFAlg_H
