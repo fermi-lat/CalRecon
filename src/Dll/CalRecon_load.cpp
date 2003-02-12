1575a0456b70c4c428ddffab5a3993b879f7e458
richard <>
2003-02-12 03:29:53 +0000

    intermediate checkin for Cluster tool. Still fiddling with internals and for test on linux

--- src/Dll/CalRecon_load.cpp
+++ src/Dll/CalRecon_load.cpp
@@ -3,15 +3,21 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-* $Header: /nfs/slac/g/glast/ground/cvs/CalRecon/src/Dll/CalRecon_load.cpp,v 1.8 2002/05/20 19:26:59 chehtman Exp $
+* $Header: /nfs/slac/g/glast/ground/cvs/CalRecon/src/Dll/CalRecon_load.cpp,v 1.9 2002/06/13 21:47:13 chehtman Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
+#include "GaudiKernel/IToolFactory.h"
+
+#define DLL_DECL_TOOL(x)       extern const IToolFactory& x##Factory; x##Factory.addRef();
+
+#define DLL_DECL_TOOL(x)       extern const IToolFactory& x##Factory; x##Factory.addRef();
 
 DECLARE_FACTORY_ENTRIES(CalRecon) {
     DECLARE_ALGORITHM( CalXtalRecAlg);
     DECLARE_ALGORITHM( CalClustersAlg);
     DECLARE_ALGORITHM( CalDisplay);
+    DLL_DECL_TOOL( SingleClusterTool );
 } 
 
 
