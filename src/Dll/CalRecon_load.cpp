437fc9b90bcdda24ad4d97c793f1fe8b372c384a
musat <>
2003-07-01 08:18:54 +0000

    Adding FuzzyClusterTool

--- src/Dll/CalRecon_load.cpp
+++ src/Dll/CalRecon_load.cpp
@@ -3,7 +3,7 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-* $Header: /nfs/slac/g/glast/ground/cvs/CalRecon/src/Dll/CalRecon_load.cpp,v 1.11 2003/02/13 23:45:08 richard Exp $
+* $Header: /nfs/slac/g/glast/ground/cvs/CalRecon/src/Dll/CalRecon_load.cpp,v 1.12 2003/02/24 16:15:42 richard Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
@@ -18,6 +18,7 @@ DECLARE_FACTORY_ENTRIES(CalRecon) {
     DECLARE_ALGORITHM( CalClustersAlg);
     DECLARE_ALGORITHM( CalDisplay);
     DLL_DECL_TOOL( SingleClusterTool );
+    DLL_DECL_TOOL( FuzzyClusterTool );
     DLL_DECL_TOOL( LastLayerCorrTool );
     DLL_DECL_TOOL( ProfileTool );
     DLL_DECL_TOOL( CalValsCorrTool );
