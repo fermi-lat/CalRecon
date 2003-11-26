df6a72cf6a5165ceeb8a521d8904f5d2f7d85dfc
berrie <>
2003-11-26 17:57:53 +0000

    comment out Gabriel's call temporarily

--- src/Dll/CalRecon_load.cpp
+++ src/Dll/CalRecon_load.cpp
@@ -3,7 +3,7 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-* $Header: /nfs/slac/g/glast/ground/cvs/CalRecon/src/Dll/CalRecon_load.cpp,v 1.12 2003/02/24 16:15:42 richard Exp $
+* $Header: /nfs/slac/g/glast/ground/cvs/CalRecon/src/Dll/CalRecon_load.cpp,v 1.13 2003/07/01 08:14:39 musat Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
@@ -18,7 +18,7 @@ DECLARE_FACTORY_ENTRIES(CalRecon) {
     DECLARE_ALGORITHM( CalClustersAlg);
     DECLARE_ALGORITHM( CalDisplay);
     DLL_DECL_TOOL( SingleClusterTool );
-    DLL_DECL_TOOL( FuzzyClusterTool );
+    /*    DLL_DECL_TOOL( FuzzyClusterTool );*/
     DLL_DECL_TOOL( LastLayerCorrTool );
     DLL_DECL_TOOL( ProfileTool );
     DLL_DECL_TOOL( CalValsCorrTool );
