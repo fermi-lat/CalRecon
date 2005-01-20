67a4e5e267febafea46fd4218ad288a85391a798
chamont <>
2005-01-20 16:11:05 +0000

    remove fuzzy clustering, which stays in its own branch

--- src/Dll/CalRecon_load.cpp
+++ src/Dll/CalRecon_load.cpp
@@ -3,7 +3,7 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-* $Header: /nfs/slac/g/glast/ground/cvs/CalRecon/src/Dll/CalRecon_load.cpp,v 1.15.4.1 2004/12/09 23:00:01 chamont Exp $
+* $Header: /nfs/slac/g/glast/ground/cvs/CalRecon/src/Dll/CalRecon_load.cpp,v 1.16 2005/01/20 15:52:20 chamont Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
@@ -14,7 +14,6 @@ DECLARE_FACTORY_ENTRIES(CalRecon) {
     DECLARE_ALGORITHM( CalClustersAlg);
     DECLARE_ALGORITHM( CalDisplay);
     DECLARE_TOOL( SingleClusterTool );
-    DECLARE_TOOL( FuzzyClusterTool );
     DECLARE_TOOL( SimpleClusterTool );
     DECLARE_TOOL( LastLayerCorrTool );
     DECLARE_TOOL( ProfileTool );
