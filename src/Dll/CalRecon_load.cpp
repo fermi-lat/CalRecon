fd208213d3ec1f3439cca508c78ccdfe8b10dd60
chamont <>
2005-03-25 09:14:57 +0000

    bug fixes, energy corrections made optional, standard test made independent from TkrRecon (because TkrRecon output is precomputed and stored in the recon root file

--- src/Dll/CalRecon_load.cpp
+++ src/Dll/CalRecon_load.cpp
@@ -3,18 +3,19 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-* $Header: /nfs/slac/g/glast/ground/cvs/CalRecon/src/Dll/CalRecon_load.cpp,v 1.18 2005/01/21 15:02:05 chamont Exp $
+* $Header: /nfs/slac/g/glast/ground/cvs/CalRecon/src/Dll/CalRecon_load.cpp,v 1.19 2005/01/25 07:51:09 chamont Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
 #include "GaudiKernel/IToolFactory.h"
 
-DECLARE_FACTORY_ENTRIES(CalRecon) {
-    DECLARE_ALGORITHM( CalXtalRecAlg);
-    DECLARE_ALGORITHM( CalClustersAlg);
-    DECLARE_ALGORITHM( CalDisplay);
-    DECLARE_TOOL( CalSingleClusteringTool );
-    DECLARE_TOOL( CalSimpleClusteringTool );
+DECLARE_FACTORY_ENTRIES( CalRecon ) {
+    DECLARE_ALGORITHM( CalXtalRecAlg ) ;
+    DECLARE_ALGORITHM( CalClustersAlg ) ;
+    DECLARE_ALGORITHM( CalDisplay ) ;
+    DECLARE_ALGORITHM( PropertiesCheckAlg ) ;
+    DECLARE_TOOL( CalSingleClusteringTool ) ;
+    DECLARE_TOOL( CalSimpleClusteringTool ) ;
     DECLARE_TOOL( LastLayerCorrTool );
     DECLARE_TOOL( ProfileTool );
     DECLARE_TOOL( CalValsCorrTool );
