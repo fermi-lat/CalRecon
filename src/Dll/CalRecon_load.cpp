b33044b39dd6627768a65dd5f4b12f9a6ae0a256
fewtrell <>
2005-04-21 21:48:21 +0000

    moved CalXtalRecAlg to CalXtalResponse, as it makes more sense from package update point of view

--- src/Dll/CalRecon_load.cpp
+++ src/Dll/CalRecon_load.cpp
@@ -3,14 +3,13 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-* $Header: /nfs/slac/g/glast/ground/cvs/CalRecon/src/Dll/CalRecon_load.cpp,v 1.18 2005/01/21 15:02:05 chamont Exp $
+* $Header: /nfs/slac/g/glast/ground/cvs/CalRecon/src/Dll/CalRecon_load.cpp,v 1.19 2005/01/25 07:51:09 chamont Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
 #include "GaudiKernel/IToolFactory.h"
 
 DECLARE_FACTORY_ENTRIES(CalRecon) {
-    DECLARE_ALGORITHM( CalXtalRecAlg);
     DECLARE_ALGORITHM( CalClustersAlg);
     DECLARE_ALGORITHM( CalDisplay);
     DECLARE_TOOL( CalSingleClusteringTool );
--- src/mainpage.h
+++ src/mainpage.h
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/CalRecon/src/mainpage.h,v 1.11 2005/03/02 07:20:53 fewtrell Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/CalRecon/src/mainpage.h,v 1.11.2.1 2005/03/25 21:58:18 fewtrell Exp $
 // Mainpage for doxygen
 
 /** @mainpage package CalRecon
@@ -10,19 +10,12 @@
  *  CalRecon package reconstructs the energy and direction  of  incident particle from
  *   the calorimeter information. 
  *   
- *   Package contains 3 algorithms: CalXtalRecAlg, CalClustersAlg
- *   and CalDisplay.
+ *   Package contains 2 algorithms: CalClustersAlg and CalDisplay.
  *
  *   The  control flow diagram of CalRecon package, including also the CalDigi package,
  *   is given on the following WEB page:
  * <A HREF="http://www-glast.slac.stanford.edu/software/CAL/meetings/calrecondiagram.pdf"> CalRecon diagram </A> 
  *
- *   CalXtalRecAlg takes the digitized calorimeter information from CalDigiCol
- *   as input, calculates the energy and position in each hitted crystal
- *   and stores this data into CalXtalRecCol.  CalXtalResponse package is 
- *   used for the estimation of energy & position from digi info.  See
- *   documentation in CalXtalResponse for details.
- *  
  *   CalClustersAlg calculates the energy, position and direction for
  *   calorimeter clusters and applies energy corrections.
  *   Actually there is no real clustering algorithm implemented,
@@ -89,11 +82,6 @@
  * @param CalClustersAlg.profileToolName
  *        name of tool performing profile fitting energy correction
  *
- *
- * @param CalXtalRecAlg.xtalEneTool
- *        name of CalXtalResponse/IXtalEneTool based tool performing xtal digi->energy conversion (default is "XtalEneTool")
- * @param CalXtalRecAlg.xtalPosTool
- *        name of CalXtalResponse/IXtalPosTool based tool performing xtal digi->position conversion (default is "XtalPosTool")
  *
  * <hr>
  * @section notes release notes
