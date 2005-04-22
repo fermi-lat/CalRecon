e50514b4ab6046eda55232900921585d3607825f
chamont <>
2005-04-22 06:38:01 +0000

    transfer CalXtalRecAlg into CalXtalresponse

--- src/Dll/CalRecon_load.cpp
+++ src/Dll/CalRecon_load.cpp
@@ -3,14 +3,12 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-* $Header: /nfs/slac/g/glast/ground/cvs/CalRecon/src/Dll/CalRecon_load.cpp,v 1.21 2005/04/11 13:28:51 chamont Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
 #include "GaudiKernel/IToolFactory.h"
 
 DECLARE_FACTORY_ENTRIES( CalRecon ) {
-    DECLARE_ALGORITHM( CalXtalRecAlg ) ;
     DECLARE_ALGORITHM( CalClustersAlg ) ;
     DECLARE_ALGORITHM( CalDisplay ) ;
     DECLARE_ALGORITHM( PropertiesCheckAlg ) ;
