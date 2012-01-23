1fea61ff0922904669789bab0e08362ceea30fe3
jrb <>
2012-01-23 19:50:51 +0000

    1. MinuitLibs already covered. 2. For component, all clients are 'depsOnly'

--- CalReconLib.py
+++ CalReconLib.py
@@ -1,8 +1,7 @@
-#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/CalRecon/CalReconLib.py,v 1.2 2008/09/25 21:41:14 ecephas Exp $
+#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/CalRecon/CalReconLib.py,v 1.3 2009/11/13 00:46:49 jrb Exp $
 def generate(env, **kw):
-    if not kw.get('depsOnly', 0):
-        env.Tool('addLibrary', library = ['CalRecon'])
-    env.Tool('addLibrary', library = env['minuitLibs'])
+    #if not kw.get('depsOnly', 0):
+    #    env.Tool('addLibrary', library = ['CalRecon'])
     env.Tool('CalXtalResponseLib')
     env.Tool('TkrUtilLib')
     env.Tool('CalUtilLib')
